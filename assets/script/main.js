02d96a13144341cb6b7b6e59e2ca287d6f7390c0
RubenErhardt <rubengamebattles@outlook.com>
2023-12-12 13:07:46 +0100

    comeonnn

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,54 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+document.querySelectorAll('section a').forEach(function(link) {
+  link.addEventListener('click', function() {
+  
+    link.classList.toggle('clicked');
+  });
+});
+
+document.querySelectorAll('section a').forEach(function(link) {
+  link.addEventListener('dblclick', function() {
+    link.classList.toggle('dbclicked');
+  });
+});
+
+function random_bg_color() {
+  var x = Math.floor(Math.random() * 256);
+  var y = Math.floor(Math.random() * 256);
+  var z = Math.floor(Math.random() * 256);
+  return "rgb(" + x + "," + y + "," + z + ")";
+}
+
+document.querySelectorAll('section a').forEach(function(link) {
+  link.addEventListener('keydown', function() {
+    link.style.backgroundColor = random_bg_color();
+  });
+});
+
+document.querySelectorAll('section a').forEach(function(link) {
+  let pressTimer;
+
+  link.addEventListener('mousedown', function() {
+    pressTimer = setTimeout(function() {
+      link.classList.toggle('longpressed');
+    }, 1000); // Set your desired duration (in milliseconds) for a long press
+  });
+
+  link.addEventListener('mouseup', function() {
+    clearTimeout(pressTimer);
+  });
+
+  // Handle touch events for mobile devices
+  link.addEventListener('touchstart', function() {
+    pressTimer = setTimeout(function() {
+      link.classList.toggle('longpressed');
+    }, 1000); // Set your desired duration (in milliseconds) for a long press
+  });
+
+  link.addEventListener('touchend', function() {
+    clearTimeout(pressTimer);
+  });
+});
